a2da7c56ba3707821d0387af70ec7b5f4745931a
Holger Ludvigsen <holger.ludvigsen@bekk.no>
2017-03-22 10:05:28 +0100

    Made line segments that are extended in world space

--- main.js
+++ main.js
@@ -33,13 +33,31 @@ function main() {
 	var material = new THREE.ShaderMaterial({
 		uniforms: uniforms,
 		vertexShader: document.getElementById('vertexshader').textContent,
-		fragmentShader: document.getElementById('fragmentshader').textContent
+		fragmentShader: document.getElementById('fragmentshader').textContent,
+		side: THREE.DoubleSide
 	});
 	window.uniforms = uniforms;
 
 	var grid = new THREE.LineSegments(geometry, material);
 	grid.rotation.y = Math.PI / 4;
 	scene.add(grid);
+
+	//--
+
+	var geometry = makeExtendedGeometry();
+	window.uniforms = uniforms;
+
+	var material = new THREE.ShaderMaterial({
+		uniforms: uniforms,
+		vertexShader: document.getElementById('extendedVertexshader').textContent,
+		fragmentShader: document.getElementById('fragmentshader').textContent,
+		side: THREE.DoubleSide
+	});
+
+	var lines = new THREE.Mesh(geometry, material);
+	scene.add(lines);
+
+	//--
 
 	setupParameters();
 
@@ -91,10 +109,77 @@ function makeGeometry() {
 	return geometry;
 }
 
+function makeExtendedGeometry() {
+	var geometry = new THREE.BufferGeometry();
+
+	var linePoints = [
+		new THREE.Vector3(0,0,0),
+		new THREE.Vector3(0,1,0),
+		new THREE.Vector3(1,1,0),
+		new THREE.Vector3(-2,0,-2),
+		new THREE.Vector3(1,-1,1)
+	];
+
+	var positions = [];
+	var indices = [];
+	var miters = [];
+
+	var p00 = linePoints[0].clone();
+	var p01 = linePoints[0].clone();
+
+	//p00 = p00.clone().add(new THREE.Vector3(1,1,1).multiplyScalar(-0.02));
+	//p01 = p01.clone().add(new THREE.Vector3(1,1,1).multiplyScalar(0.02));
+	
+	positions.push(p00, p01);
+	miters.push(-1, 1);
+
+	for (var i in linePoints) {
+		if (i == 0) continue;
+
+		var p00 = linePoints[i-1].clone();
+		var p01 = linePoints[i-1].clone();
+		var p10 = linePoints[i].clone();
+		var p11 = linePoints[i].clone();
+
+		/*
+		p00 = p00.clone().add(new THREE.Vector3(1,1,1).multiplyScalar(-0.02));
+		p01 = p01.clone().add(new THREE.Vector3(1,1,1).multiplyScalar(0.02));
+		p10 = p10.clone().add(new THREE.Vector3(1,1,1).multiplyScalar(-0.02));
+		p11 = p11.clone().add(new THREE.Vector3(1,1,1).multiplyScalar(0.02));
+		*/
+
+		positions.push(p10, p11);
+
+		var i00 = 2*i-2;
+		var i01 = 2*i-1;
+		var i10 = 2*i;
+		var i11 = 2*i+1;
+
+		indices.push(i00, i10, i11);
+		indices.push(i00, i11, i01);
+
+		miters.push(-1, 1);
+	}
+
+	var flattenPositions = [].concat.apply([], positions.map(v => [v.x, v.y, v.z]));
+
+	geometry.setIndex(new THREE.BufferAttribute(new Uint16Array(indices), 1));
+	geometry.addAttribute('position', new THREE.BufferAttribute(new Float32Array(flattenPositions), 3));
+	geometry.addAttribute('miter', new THREE.BufferAttribute(new Float32Array(miters), 1));
+
+	geometry.computeBoundingSphere();
+
+	return geometry;
+}
+
 function animate() {
 	requestAnimationFrame(animate);
 
-	window.uniforms.time.value = new Date().getTime() - timeStart;
+	var relativeTime = (new Date().getTime() - timeStart) / 1000;
+	window.uniforms.time.value = relativeTime;
+
+	camera.position.set(5*Math.sin(relativeTime), 2, 5*Math.cos(relativeTime))
+	camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 	render();
 }
